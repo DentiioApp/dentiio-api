54f330b9ea0f49e67163ccfbbf74e680a0adbd35
Lory LETICEE <loryleticee@gmail.com>
2020-09-23 03:35:25 +0200

    add name to roups missed

--- api/src/Entity/Pathologie.php
+++ api/src/Entity/Pathologie.php
@@ -26,7 +26,7 @@ class Pathologie
 
     /**
      * @ORM\Column(type="string", length=45)
-     * @Groups({"clinicalcase_read"})
+     * @Groups({"clinicalcase_read", "pathologie_read"})
      */
     private $name;
 
